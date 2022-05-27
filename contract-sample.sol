3d44fab534a96e0c20c7fb6b58730dd0c297b9b4
0xwata <watarutakamine@gmail.com>
2022-05-28 02:02:49 +0900

    update base_uri from master to main

--- contract-sample.sol
+++ contract-sample.sol
@@ -13,7 +13,7 @@ contract Kurama is ERC721, Ownable {
     constructor() ERC721("Kurama", "KRM") {}
 
     function _baseURI() internal pure override returns (string memory) {
-        return "https://raw.githubusercontent.com/0xwata/scribble-smart-contract/master/meta/";
+        return "https://raw.githubusercontent.com/0xwata/scribble-smart-contract/main/meta/";
     }
 
     function safeMint(address to) public onlyOwner {
